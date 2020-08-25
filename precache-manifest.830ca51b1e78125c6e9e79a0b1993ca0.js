abaca0d8a9e1bbac702f37bb5d88fbb0a26095e1
Eric Li <ericut@gmail.com>
2020-08-25 09:51:21 -0300

    Updates

---
+++ precache-manifest.830ca51b1e78125c6e9e79a0b1993ca0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a7838de3d11334db47b4f2647ba2b6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d4788fb3a5c01afc796",
+    "url": "/static/js/2.88c3f30b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.88c3f30b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7509e14010a28705434",
+    "url": "/static/js/main.072f4430.chunk.js"
+  },
+  {
+    "revision": "af6554ab81b58153106e",
+    "url": "/static/js/runtime-main.aa173224.js"
+  }
+]);
